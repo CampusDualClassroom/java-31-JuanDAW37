45f182a59d05d79b1a999eeb197df11b1c6f677b
JuanDAW37 <jymtr1968@gmail.com>
2024-10-17 10:48:37 +0200

    Exercicio 31 listo

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,25 @@
 package com.campusdual.classroom;
 
+import java.io.*;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
+        String file = "D:\\IdeaProjects\\java-31-JuanDAW37\\src\\main\\resources\\lorem.txt";
+        readFile(file);
+    }
 
+    public static void readFile(String file) {
+        BufferedReader buffer = null;
+        try{
+            String read ="";
+            buffer = new BufferedReader(new FileReader(new File(file)));
+            while ((read = buffer.readLine())!=null){
+                System.out.println(read);
+            }
+            buffer.close();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
